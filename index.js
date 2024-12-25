f02a441dee6cb2c95f26a56693ea9744799b5a7e
srinivas kannan <srinivaskannan22@gmail.com>
2024-12-25 22:54:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const a=document.getElementsByClassName("about");
-const b=document.getElementsByClassName("experience");
-const c=document.getElementsByClassName("contact");
+const a = document.getElementsByClassName("about");
+const b = document.getElementsByClassName("experience");
+const c = document.getElementsByClassName("contact");
 
-a.style.display=none;
-b.display=none;
-c.display=none;
+// Set all sections to hidden initially
+if (a[0]) a[0].style.display = "block";
+if (b[0]) b[0].style.display = "none";
+if (c[0]) c[0].style.display = "none";
 
-function about(){
-        const b=document.getElementsByClassName("experience");
-        const c=document.getElementsByClassName("contact");
-        b.style.display="none";
-        c.style.display="none";
-
+// Function to show "About" and hide others
+function about() {
+    if (b[0]) b[0].style.display = "none"; // Hide "experience"
+    if (c[0]) c[0].style.display = "none"; // Hide "contact"
+    if (a[0]) a[0].style.display = "block"; // Show "about"
+}
+function experience() {
+    if (b[0]) b[0].style.display = "block"; // Hide "experience"
+    if (c[0]) c[0].style.display = "none"; // Hide "contact"
+    if (a[0]) a[0].style.display = "none"; // Show "about"
+}
+function contact() {
+    if (b[0]) b[0].style.display = "none"; // Hide "experience"
+    if (c[0]) c[0].style.display = "block"; // Hide "contact"
+    if (a[0]) a[0].style.display = "none"; // Show "about"
 }
